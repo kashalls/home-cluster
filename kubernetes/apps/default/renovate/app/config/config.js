fd5f871b4a2faf18f9e09b45eb3ca65e7f5c09ea
Jordan Jones <jordpjones@gmail.com>
2023-10-12 12:05:00 -0700

    chore(renovate): add custom config

---
+++ kubernetes/apps/default/renovate/app/config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // https://docs.renovatebot.com/self-hosted-configuration
+
+  // Customization
+  "username": "moonami-bot[bot]",
+  "dependencyDashboardTitle": "Moonami Dashboard 🌙",
+  "gitAuthor": "Moonami Bot <138733633+moonami-bot[bot]@users.noreply.github.com>",
+
+  // Prefered options
+  "platform": "github",
+  "requiredConfig": "optional",
+  "extends": [
+    'config:best-practices',
+    ':gitSignOff'
+  ],
+
+  // Commit Messages
+  "commitMessageTopic": "{{depName}}",
+  "commitMessageExtra": "to {{newVersion}}",
+  "commitMessageSuffix": "",
+
+  "packageRules": [
+    {
+      "matchDatasources": ["helm"],
+      "commitMessageTopic": "chart {{depName}}"
+    },
+    {
+      "matchDatasources": ["docker"],
+      "commitMessageTopic": "image {{depName}}"
+    }
+  ]
+}
