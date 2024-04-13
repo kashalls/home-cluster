a662f6162f0897d49fe2e961b38fe1c4e09d6f3c
Jordan Jones <jordpjones@gmail.com>
2024-04-13 05:14:40 -0700

    test

--- pulumi/cloudflare/src/bucket.ts
+++ pulumi/cloudflare/src/bucket.ts
@@ -9,6 +9,7 @@ const r2_cloudnative_pg_v1 = new cloudflare.R2Bucket("r2-cloudnative-pg-v1", {
     protect: true,
 });
 
+
 const r2_volsync_restic = new cloudflare.R2Bucket("r2-volsync-restic", {
   accountId: process.env.CLOUDFLARE_ACCOUNT_TAG,
   location: "WNAM",
