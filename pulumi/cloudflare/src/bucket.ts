c80f4a4d4424fd327ec54a58c1e942911fbd893b
Jordan Jones <jordpjones@gmail.com>
2024-04-14 01:23:35 -0700

    test #3

--- pulumi/cloudflare/src/bucket.ts
+++ pulumi/cloudflare/src/bucket.ts
@@ -15,3 +15,9 @@ export const r2_volsync_restic = new cloudflare.R2Bucket("r2-volsync-restic", {
 }, {
   protect: true,
 });
+
+export const r2_test = new cloudflare.R2Bucket("r2-test", {
+  accountId: "3ed51f2c99510cd7fe8fcd8ed0621a37",
+  location: "WNAM",
+  name: "test-kashall",
+});
